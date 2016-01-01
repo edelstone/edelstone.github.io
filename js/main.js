74e9e01f3138bc5b2ab17b1941882eeb27a71a0a
Michael Edelstone <michael.edelstone@gmail.com>
2016-01-01 17:06:15 -0600

    mobile nav set up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 // No 300ms mobile delay on mobile
-
 window.addEventListener('load', function() {
 	FastClick.attach(document.body);
 }, false);
+
+
+// Navigation drawer
+$(document).ready(function() {
+  $('.drawer').drawer();
+});
