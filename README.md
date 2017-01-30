cf6319a8419d2a4fd21a30a833345ff033afd97f
Michael Edelstone <michael.edelstone@gmail.com>
2017-01-30 16:45:42 -0600

    Pushing new 2017 website and portfolio

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# edelstone.github.io
-I moved my website to [michaeledelstone.com](http://michaeledelstone.com).
+# my-website
+
+This is the source code for my personal website at [michaeledelstone.com](http://michaeledelstone.com).
+
+Thanks for looking.
---
+++ js/main.js
@@ -0,0 +1,49 @@
+// Remove 300ms tapping delay on mobile
+window.addEventListener('load', function() {
+	FastClick.attach(document.body);
+}, false);
+
+// Get and display page load time
+// calculate the time before calling the function in window.onload
+var beforeload = (new Date()).getTime();
+
+function getPageLoadTime(){
+	// calculate the current time in afterload
+	var afterload = (new Date()).getTime();
+	// now use the beforeload and afterload to calculate the seconds and round to 2 decimals max
+	seconds = ((afterload-beforeload) / 1000).toFixed(2);
+	// Place the seconds in the innerHTML to show the results
+	$(".load-time").text('This page loaded in ' + seconds + ' seconds');
+}
+
+window.onload = getPageLoadTime;
+
+// Featherlight modal customizations
+$.extend($.featherlight.defaults, {
+   closeOnClick: 'anywhere'
+});
+
+//Smooth scroll to anchors
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 700);
+    return false;
+});
+
+//Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("panel-show");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+  	  panel.style.maxHeight = null;
+    } else {
+  	  panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
